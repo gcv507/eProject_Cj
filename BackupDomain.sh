8c3a87977f1076e6720bd1ea5f97cbb4bbb97919
Chiranjeev <gcv507@gmail.com>
2020-03-23 16:31:24 -0700

    First Commit

---
+++ BackupDomain.sh
@@ -0,0 +1,44 @@
+#...............................................................
+# Backup Domain Script
+#
+# This script will backup the PowerCenter Domain to
+# $INFA_SHARED/Backup folder
+#
+# Created by: Keshav Vadrevu, IPS
+# Created on: March 22, 2011
+# Modified by: Vijayanand Silvary
+# Modified on: October 3, 2011
+#...............................................................
+RUN_USER=`whoami`
+if [ $RUN_USER = 'infaadmn' ] ; then
+temp_var=1
+else
+echo "Warning!! You cannot execute this script with the current user. Please login as infa951 and execute this script"
+exit -1
+fi
+
+BACKUP_DIR=$INFA_SHARED/Backup/da_backups
+timestamp=`date +%m%d%Y`
+BACKUP_FILE=$INFA_DOMAIN.$timestamp
+find $BACKUP_DIR -name "$INFA_DOMAIN.*.mrep.gz" -mtime \+14 -exec rm -f {} \;
+clear
+echo "
+...............................................................
+PowerCenter Domain Backup Process
+...............................................................
+
+     Domain Name: $INFA_DOMAIN
+     Backup Dir: $BACKUP_DIR
+...............................................................
+
+Please wait...
+
+"
+infasetup.sh backupdomain -f -da $REP_DATABASE_HOST_NAME:$REP_DATABASE_PORT_NUMBER -du $INFA_DEFAULT_DATABASE_USER -dt $REP_DATABASE_TYPE -bf $BACKUP_DIR/$BACKUP_FILE -dn $INFA_DOMAIN -ds $REP_DATABASE_SERVICE_NAME
+RC=$?
+if [ $RC != 0 ] ; then
+echo "$INFA_DOMAIN backup failed" | mailx -s "Alert! Informatica Development Domain backup failed" TWDAINFSupport@timewarner.com
+fi
+gzip $BACKUP_DIR/$BACKUP_FILE.mrep
+exit 0
+~
